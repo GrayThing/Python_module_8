3ae6acac66bccca1d1fad9d3bb0d8ee71e3eec85
GrayThing <samorodov-artem@mail.ru>
2024-09-24 14:50:47 +0300

    Выполнено задание: 2023/11/26 00:00|Домашнее задание по теме "Создание исключений".

---
+++ module_8_3.py
@@ -0,0 +1,33 @@
+class Car:
+    def __init__(self, model: str, vin: int, numbers: str ):
+        self.model = model
+        if self.__is_valid_vin(vin):
+            self.__vin = vin
+        if self.__is_valid_numbers(numbers):
+            self.__numbers = numbers
+
+    def __is_valid_vin(self, vin_number: int):
+        if vin_number not in range(1000000, 9999999):
+            raise IncorrectVinNumber(f'Vin number must be in range 1000000 - 9999999, got {vin_number}')
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber(f'Expected class int, got {type(vin_number).__name__} instead')
+        return True
+
+    def __is_valid_numbers(self, numbers: str):
+        if len(numbers) != 6:
+            raise IncorrectCarNumbers(f'Numbers must be equal 6, got {len(numbers)}')
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers(f'Expected class str, got {type(numbers).__name__} instead')
+        return True
+
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
